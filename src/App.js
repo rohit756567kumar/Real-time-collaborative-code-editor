397b3d004dbf77b9fc879a5cf54b95ec8f37adee
Rohit Kumar <rohit756567@gmail.com>
2025-09-23 17:45:09 +0530

    Add pop Up and unique Id and some important features

--- src/App.js
+++ src/App.js
@@ -2,16 +2,31 @@ import './App.css';
 import {BrowserRouter , Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
 import CodePage from './pages/codePage';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <>
-     <BrowserRouter>
-     <Routes>
-      <Route path='/' element={<Home/>}> </Route>
-      <Route path='/code/:roomId' element={<CodePage/>}></Route>
-     </Routes>
-     </BrowserRouter>
+      <div>
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            success: {
+              theme: {
+                primary: "#d45c1b",
+              },
+            },
+          }}
+        ></Toaster>
+      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            {" "}
+          </Route>
+          <Route path="/code/:roomId" element={<CodePage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,72 @@
 import React from "react";
+import {v4 as uuidv4} from 'uuid';
+import { useState } from "react";
+import toast from 'react-hot-toast';
+import { useNavigate } from "react-router-dom";
 
 const Home = () =>{
+  const navigate = useNavigate();
+  const [roomId, setRoomId] = useState('');
+  const [username, setUsername] = useState('');
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidv4();
+    setRoomId(id);
+    toast.success('Created a new room');
+  }
+
+  const joinRoom = () => {
+    if(!roomId || !username){
+      toast.error('ROOM ID & username is required');
+      return;
+    }
+
+    //Redirect
+    navigate(`/code/${roomId}`,{
+      state: {
+        username,
+      }
+    })
+  }
+
+  const handleInputEnter = (e) => {
+    if(e.code === 'Enter'){
+      joinRoom();
+    }
+  }
+
     return (
       <div className="homePageWrapper">
         <div className="formWrapper">
-          <img src="/CodeX.png" width="400px" height="100px" alt="CodeX logo" />
+          <img className="homePageLogo" src="/CodeX.png" alt="CodeX logo" />
           <h4 className="mainLabel"> Paste invitation Room ID</h4>
           <div className="inputGroup">
             <input
               type="text"
               className="inputBox"
               placeholder="Room ID"
+              onChange={(e) => {
+                setRoomId(e.target.value);
+              }}
+              value={roomId}
+              onKeyUp={handleInputEnter}
             ></input>
             <input
               type="text"
               className="inputBox"
               placeholder="Username"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+              value={username}
+              onKeyUp={handleInputEnter}
             ></input>
-            <button className="btn joinbtn">Join</button>
+            <button className="btn joinbtn" onClick={joinRoom}>
+              Join
+            </button>
             <span className="creatInfo">
-              if you don'y have an invite then create
-              <a href=" " className="createNewBtn">
+              if you don't have an invite then create{"  "}
+              <a onClick={createNewRoom} href=" " className="createNewBtn">
                 New Room
               </a>
             </span>
