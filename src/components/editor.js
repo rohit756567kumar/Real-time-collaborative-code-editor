cf1e952302433b4651cf5fcdbb18f1a91145f2c9
Rohit Kumar <rohit756567@gmail.com>
2025-09-24 19:01:09 +0530

    Editor page Frontend

---
+++ src/components/editor.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const editor = () => {
+    return(
+        <textarea id="realtimeEditor"></textarea>
+    )
+}
+
+export default editor;
--- src/pages/codePage.js
+++ src/pages/codePage.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import Client from "../components/Client";
+import Editor from "../components/editor";
 
 const CodePage = () =>{
+    const [clients, setClients] = useState([
+        {socketId: 1, username: 'Rohit'},
+        {socketId: 2, username: 'Jon'}
+    ]);
     return(
-        <div>Code Page</div>
+        <div className="mainWrap">
+            <div className="aside">
+                <div className="asideInner">
+                    <div className="logo">
+                        <img className="logoImage" src="/CodeX.png" alt="logo"></img>
+                    </div>
+                    <h3>Connected</h3>
+                    <div className="clientsList">
+                        {
+                            clients.map(client => (
+                                <Client key={client.socketId} username={client.username}/>
+                            ))
+                        }
+                    </div>
+                </div>
+                <button className="btn copyBtn">Copy Room ID</button>
+                <button className="btn leaveBtn">Leave</button>
+            </div>
+            <div className="codeWrap">
+                <Editor />
+            </div>
+        </div>
     )
 }
 
